1fac9401b7dfd280520ccb0c434c6f6766c03844
DariaZielinska <dari.zielinska@gmail.com>
2019-01-24 15:14:58 +0100

    validation

--- src/js/index.js
+++ src/js/index.js
@@ -14,9 +14,54 @@ document.addEventListener("DOMContentLoaded", function () {
             welcomeButton.classList.remove("hidden");
         })
 
+    });
+
+    form.addEventListener("submit", function (e) {
+
+        const email = document.querySelector("input[type='email']").value;
+        const password = document.querySelector("input[type='password']").value;
+        const agree = document.querySelector("input[type='checkbox']").checked;
+        let error = false;
+        let errorMessages = [];
+        const errorMessageContainer = document.querySelector("#error-container");
+        const endButton = document.querySelector("#end-popup-form");
+
+        if (email.indexOf("@") === -1){
+            error = true;
+            errorMessages.push("Please enter a valid e-mail address");
+        }
+
+        if(password.length === 0){
+            error = true;
+            errorMessages.push("Field password is required");
+        }
+
+        if(!agree){
+            error = true;
+            errorMessages.push("You need to agree terms");
+        }
+
+        if(error === true){
+            e.preventDefault();
+            errorMessageContainer.innerHTML = null;
+
+            errorMessages.forEach(function (message) {
+                const p = document.createElement("p");
+                p.innerText = message;
+                errorMessageContainer.appendChild(p);
+            })
+        }else{
+            e.preventDefault();
+            errorMessageContainer.innerHTML = "Loading...";
+            setTimeout(function(){
+                form.classList.add("hidden");
+                endButton.classList.remove("hidden")
+            }, 3000);
+        }
+
+
+
     })
-
-
 
 
 });
