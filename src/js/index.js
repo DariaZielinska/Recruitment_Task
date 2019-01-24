601aec62c79bb240cefc60d416ebb39d8f2ad409
DariaZielinska <dari.zielinska@gmail.com>
2019-01-24 14:01:05 +0100

    popup style

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,22 @@
-/* Here goes your JS code */
+document.addEventListener("DOMContentLoaded", function () {
+
+    const welcomeButton = document.querySelector("#show-popup-form");
+    const form = document.querySelector("#form");
+    const close = document.querySelector("i");
+
+    welcomeButton.addEventListener("click", function () {
+
+        welcomeButton.classList.add("hidden");
+        form.classList.remove("hidden");
+
+        close.addEventListener("click", function() {
+            form.classList.add("hidden");
+            welcomeButton.classList.remove("hidden");
+        })
+
+    })
+
+
+
+
+});
